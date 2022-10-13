6a562458408c230c5cfd4a68d97d2bf48a765161
Buiama <83916242+Buiama@users.noreply.github.com>
2022-10-13 10:18:15 +0200

    Add files via upload

--- Лаб_1/Lab_1_java/src/Main.java
+++ Лаб_1/Lab_1_java/src/Main.java
@@ -1,118 +1,49 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.*;
+import java.util.Random;
 
 import static java.lang.Integer.MAX_VALUE;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the name of file: ");
-        String name = scan.nextLine() + ".txt";
-        System.out.print("In which number system to create a file? (Kb/Mb/Gb): ");
-        String system = scan.nextLine().toUpperCase();
-        System.out.print("Enter file size in " + system + ": ");
-        long size = scan.nextLong();
-        long time = System.currentTimeMillis();
-        try {
-            long count = generateFile(name, system, size);
-            System.out.print("A file with the name \"" + name + "\" and size of " + size + system +
-                    " was successfully generated in ");
-            System.out.println((double) (System.currentTimeMillis() - time) + "ms!");
-            System.out.print("\nHow many files to use for sorting? N = ");
-            int n = scan.nextInt();
-//            time = System.currentTimeMillis();
-//            ArrayList<ArrayList<Integer>> arr = fibonacci(6, n-1);
-//            System.out.println(arr);
-//            System.out.println((double) (System.currentTimeMillis() - time) + "ms!");
-            int i = polyPhaseSort(name, n, count, system); // отсортированный файл
-        } catch (Exception e) {
-            System.err.println(e);
-        }
-//        File file = new File(name);
-//        Scanner reader = new Scanner(file);
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world!");
+//        int n = 4;
+//        int[] a = new int[2*n];
+//        for(int i=0;i<n;i++) {
+//            a[i] = n-i;
+//        }
+//        System.out.println(Arrays.toString(a));
+////        straightMerge(n,a);
+//        System.out.println(Arrays.toString(a));
+        polyPhaseSort();
     }
-    public static ArrayList<ArrayList<Integer>> fibonacci(int rows, int cols) {
-//        int rows = 6;
-        int sum;
-        int res = 2;
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-        for (int i = 0; i < rows; i++) {
-            res--;
-            if(res==0) {
-                res=cols+1;
+    public static void select() {
+//        int i, z;
+        if(d[j]<d[j+1]) j=j+1;
+        else {
+            if(d[j]==0) {
+                level = level+1;
+                z=a[i];
+                for (i=1;i<=n;i++) {//!!!
+                    d[i]=z+a[i+1]-a[i];
+                    a[i]=z+a[i+1];
+                }
             }
-            sum = 0;
-            arr.add(new ArrayList<>());
-            for (int j = 0; j < cols; j++) {
-                if(i == 0 && j == 0) {
-                    arr.get(i).add(1);
-                }
-                else if(i==0) {
-                    arr.get(i).add(0);
-                }
-                else if (j == cols - 1) {
-                    arr.get(i).add(j, arr.get(i - 1).get(0));
-                }
-                else {
-                    arr.get(i).add(j, arr.get(i - 1).get(0) + arr.get(i - 1).get(j + 1));
-                }
-                sum +=arr.get(i).get(j);
-            }
-            arr.get(i).add(sum);
-            arr.get(i).add(res);
+            j=0;//!!!
         }
-        return arr;
+        d[j] = d[j]-1;
     }
-
-    public static int polyPhaseSort(String name, int n, long count, String system) throws IOException {
-        for(int i=1;i<n;i++) {
-            File file = new File("T" + i + ".txt");
-            file.createNewFile();
-        }
-        ArrayList<ArrayList<Integer>> fibList = fibonacci(20, n-1);
-        BufferedReader reader = Files.newBufferedReader(Path.of(name), StandardCharsets.UTF_8);
-        File file = new File(name);
-        long time = System.currentTimeMillis();
-        int gig = 500000000;//java -XX:+PrintFlagsFinal -version | findstr /i "HeapSize PermSize ThreadStackSize"
-        int k=0;
-//        switch (system) {
-//            case "GB":
-//                size *= 1024 * 1024 * 1024;
-//                break;
-//            case "MB":
-//                size *= 1024 * 1024;
-//                break;
-//            case "KB":
-//                size *= 1024;
-//                break;
-//        }
-        while (count/fibList.get(k).get(n-1)>gig) {
-            k++;
-        }
-        System.out.println(k);
-        int[] arr = new int[500000000];
-        for(int i=0;i<50000000;i++) {
-            arr[i] = Integer.parseInt((reader.readLine()));
-        }
-        System.out.println("\nArray read = "+ (double) (System.currentTimeMillis() - time) + "ms!");
-        time = System.currentTimeMillis();
-        Arrays.parallelSort(arr);
-        System.out.println("Array.parallelSort = "+ (double) (System.currentTimeMillis() - time) + "ms!");
-//        for(int i=0;i<10;i++) {
-//            System.out.println(arrAm[i]);
-//        }
-//        System.out.println();
-//        for(int i=990;i<1000;i++) {
-//            System.out.println(arr[i]);
-//        }
-        return 1;
+    public static void copyrun(File f0, File file) {
+        do {
+            copy();
+        } while (true);//!?
     }
-
-    public static long generateFile(String name, String system, long size) throws IOException {
-        File file = new File(name);
+    public static void openRandomSeq(String system, long size) throws IOException {
+        File file = new File("name.txt");
         file.createNewFile();
         FileWriter in = new FileWriter(file);
         Random rand = new Random();
@@ -139,12 +70,237 @@ public class Main {
             count++;
         }
         in.close();
-        return count*10240;
+    }
+    public static void startWrite() {
+
+    }
+    public static void copy() {
+
+    }
+    private static void openSeq(File file, String name) {
+
+    }
+    public static final int n=6;
+    public static int i,j,z,level;
+    public static int[] a = new int[n];
+    public static int[] d = new int[n];
+    public static void polyPhaseSort() throws IOException {
+//        final int n =6;
+        int mx,tn;
+        int k,dn;
+        int x,min;
+        int[] t = new int[n];
+        int[] ta = new int[n];
+        File f0;
+        File[] f = new File[n];
+        String[] fn = new String[n];
+        int eof = 5;
+        int c=0;
+
+        FileWriter[] in = new FileWriter[n];
+
+//        openRandomSeq("gb",3);
+        f0 = new File("name.txt");
+        //listSeq();
+//            openSeq(f[i],fn[i]);
+        for (i=0;i<n;i++) {
+            fn[i]="T"+(i+1)+".txt";
+            f[i] = new File(fn[i]);
+            f[i].createNewFile();
+        }
+        for (i=0;i<n-1;i++) {
+            a[i]=1;d[i]=1;
+            //startWrite(f[i]);//!?
+            in[i]= new FileWriter(f[i]);
+        }
+        level=1;j=0;//!!!
+        a[n-1]=0;d[n-1]=0;//!!!
+//        startRead(f0);
+        BufferedReader reader = Files.newBufferedReader(f0.toPath(), StandardCharsets.UTF_8);
+        boolean eor;
+        String firstX, firstZ;
+        String[] firstY=new String[n];
+        firstX = reader.readLine();
+        do {
+            select();
+//            copyrun(f0,f[j]);
+            do {
+//                copy();
+                firstY[j]=firstX;
+                in[j].write(firstX+'\n');
+                firstX = reader.readLine();
+                eor=false;
+                if(Integer.parseInt(firstX)<Integer.parseInt(firstY[j])) eor=true;
+                c++;
+            } while (!eor);//!?
+        } while (c!=eof && (j!=n-2));//!?
+        j=0;
+        while (c!=eof) {//!?
+            select();
+            if(Integer.parseInt(firstY[j]) <= Integer.parseInt(firstX)) {
+//            copyrun(f0,f[j]);
+                do {
+//                copy();
+                    firstY[j]=firstX;
+                    in[j].write(firstX+'\n');
+                    firstX = reader.readLine();
+                    eor=false;
+                    if(Integer.parseInt(firstX)<Integer.parseInt(firstY[j])) eor=true;
+                    c++;
+                } while (!eor);//!?
+                if(c!=eof) {//!?
+                    d[j]=d[j]+1;
+                }
+//            copyrun(f0,f[j]);
+                do {
+//                copy();
+                    firstY[j]=firstX;
+                    in[j].write(firstX+'\n');
+                    firstX = reader.readLine();
+                    eor=false;
+                    if(Integer.parseInt(firstX)<Integer.parseInt(firstY[j])) eor=true;
+                } while (!eor);//!?
+            }
+//            copyrun(f0,f[j]);
+            do {
+//                copy();
+                firstY[j]=firstX;
+                in[j].write(firstX+'\n');
+                firstX = reader.readLine();
+                eor=false;
+                if(Integer.parseInt(firstX)<Integer.parseInt(firstY[j])) eor=true;
+            } while (!eor);//!?
+        }
+        BufferedReader[] readers = new BufferedReader[n];
+        for(i=0;i<n-1;i++) {//!!!
+            t[i] = i;
+//            startRead();
+            readers[i] = Files.newBufferedReader(f[i].toPath(), StandardCharsets.UTF_8);
+        }
+        t[n-1] = n-1;
+        do {
+            z=a[n-2]; d[n-1]=0;
+//            startWrite(f[t[n-1]],t[n-1]);!!!!!!!!!!!!
+            do {
+                k = 0;
+                for (i = 0; i < n - 1; i++) {//!!!
+                    if (d[i] > 0) {
+                        d[i] = d[i] - 1;
+                    } else {
+                        ta[k] = t[i]; k = k + 1;
+                    }
+                }
+                if (k == 0) {
+                    d[n-1] = d[n-1] + 1;
+                }
+                else {
+                    do {
+                        i = 0; mx = 0; min = Integer.parseInt(readers[ta[0]].readLine()); //f[ta[0]].first;
+                        while (i < k) {
+                            i = i + 1; x = Integer.parseInt(readers[ta[i]].readLine());//????
+                            if (x < min) {
+                                min = x; mx = i;
+                            }
+                        }
+//                        copy(f[ta[mx]],f[t[n-1]]);
+                        firstX=readers[ta[mx]].readLine();
+                        firstZ=firstX;
+                        in[t[n-1]].write(firstX);
+                        eor=false;
+                        if(Integer.parseInt(firstX)<Integer.parseInt(firstZ)) eor=true;
+
+                        if (eor) {
+                            for (int tx = mx; tx < k; tx++) {
+                                ta[tx] = ta[tx + 1];
+                            }
+                            k = k - 1;
+                        }
+                    } while (k != 0);
+                }
+                z = z - 1;
+            }while (z!=0);
+//            startRead(f[t[n-1]],t[n-1]);
+            tn=t[n-1];dn=d[n-1];z=a[n-2];
+            for (i=n-1;i>0;i--) {
+                t[i]=t[i-1];d[i]=d[i-1];a[i]=a[i-1]-z;
+            }
+            t[0]=tn;d[0]=dn;a[0]=z;
+//            startWrite(f[t[1]], t[n]);
+            in[t[1]].write(t[n-1]);
+            level=level-1;
+        } while (level!=0);
+        for (i=0;i<n;i++) {
+//            closeSeq(f[i]);
+            in[i].close();
+            readers[i].close();
+        }
+//        closeSeq(f0);
+        reader.close();
+        }
+
+
+
+    public static void straightMerge(int n, int[] a) {//как в учебнике!
+        int i,j, k, l,t;
+        int h,m, p ,q,r;
+        boolean up;
+
+        up = true;
+        p=1;
+        do{
+            h=1; m=n;
+            if(up) {i=0; j=n-1; k=n; l=2*n-1;}
+            else   {k=0; l=n-1; i=n; j=2*n-1;}
+            do{
+                if(m>=p) {q=p;}
+                else {q=m;}
+                m=m-q;
+                if(m>=p) {r=p;}
+                else {r=m;}
+                m=m-r;
+                while ((q!=0) && (r!=0)) {
+                    if(a[i]<a[j]){a[k]=a[i];k=k+h;i=i+1;q=q-1;}
+                    else         {a[k]=a[j];k=k+h;j=j-1;r=r-1;}
+                }
+                while (r>0)       {a[k]=a[j];k=k+h;j=j-1;r=r-1;}
+                while (q>0)       {a[k]=a[i];k=k+h;i=i+1;q=q-1;}
+                h=-h; t=k; k=l; l=t;
+            } while (m!=0);
+            up=!up;p=2*p;
+        } while (p<=n);
+        if(!up) {
+            for(i=0;i<n;i++) {
+                a[i]=a[i+n];
+            }
+        }
     }
 }
-//for (int i=0;i<10;i++) {
-//for (int j=0;j<n+1;j++) {
-//System.out.print(fibList.get(i).get(j)+" ");
-//}
-//System.out.println();
-//}
+
+
+
+
+
+
+
+
+
+    /*public static void select() {
+
+    }
+    public static void copyrun() {
+
+    }
+    public static void polyphaseSort() {
+        final int n = 6;
+        int i,j, mx, tn;
+        int k, dn, z, level;
+        int x, min;
+        int[] a = new int[n];
+        int[] d = new int[n];
+        int[] t = new int[n];
+        int[] ta = new int[n];
+        Sequence f0;
+        Sequence[] f = new Sequence[n];
+        String[] fn = new String[n];
+
+    }*/
