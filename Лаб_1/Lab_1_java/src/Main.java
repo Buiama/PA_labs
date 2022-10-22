268ac745ad2297e16468d11a201eaa52714a29af
Buiama <83916242+Buiama@users.noreply.github.com>
2022-10-23 01:10:34 +0200

    Add files via upload

--- Лаб_1/Lab_1_java/src/Main.java
+++ Лаб_1/Lab_1_java/src/Main.java
@@ -1,186 +1,175 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.Random;
+import java.util.Scanner;
 
 import static java.lang.Integer.MAX_VALUE;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
-//        int n = 4;
-//        int[] a = new int[2*n];
-//        for(int i=0;i<n;i++) {
-//            a[i] = n-i;
-//        }
-//        System.out.println(Arrays.toString(a));
-////        straightMerge(n,a);
-//        System.out.println(Arrays.toString(a));
-        polyPhaseSort();
+        n=4;
+        File f0 = new File("name.txt");
+        polyphaseSort(counter());
     }
-    public static void select() {
-//        int i, z;
-        if(d[j]<d[j+1]) j=j+1;
-        else {
-            if(d[j]==0) {
-                level = level+1;
-                z=a[i];
-                for (i=1;i<=n;i++) {//!!!
-                    d[i]=z+a[i+1]-a[i];
-                    a[i]=z+a[i+1];
-                }
-            }
-            j=0;//!!!
-        }
-        d[j] = d[j]-1;
-    }
-    public static void copyrun(File f0, File file) {
-        do {
-            copy();
-        } while (true);//!?
-    }
-    public static void openRandomSeq(String system, long size) throws IOException {
+    public static long counter() throws FileNotFoundException {
+        long count=0;
         File file = new File("name.txt");
-        file.createNewFile();
-        FileWriter in = new FileWriter(file);
-        Random rand = new Random();
-        StringBuilder s = new StringBuilder();
-        long count =0;
-        switch (system) {
-            case "GB":
-                size *= 1024 * 1024 * 1024;
-                break;
-            case "MB":
-                size *= 1024 * 1024;
-                break;
-            case "KB":
-                size *= 1024;
-                break;
-        }
-        while (file.length() < size) {
-            for (int i = 0; i < 10240; i++) {
-                s.append(rand.nextInt(MAX_VALUE));
-                s.append('\n');
-            }
-            in.write(s.toString());
-            s.setLength(0);
+        Scanner sc = new Scanner(file);
+        int g;
+        while (sc.hasNextInt()){
+            g = sc.nextInt();
             count++;
         }
-        in.close();
+        return count;
     }
-    public static void startWrite() {
-
+    public static int n,j,l;
+    public static void select(int[] a, int[] d) {
+        int i,a0;
+        if((d[j])<(d[j+1])) j++;
+        else {
+            if(d[j]==0) {
+                l++;
+                a0=a[0];
+                for (i=0;i<n-1;i++) {
+                    d[i]=a0+a[i+1]-a[i];
+                    a[i] = a0 + a[i + 1];
+                }
+            }
+            j=0;
+        }
+        d[j] = d[j] - 1;
     }
-    public static void copy() {
-
-    }
-    private static void openSeq(File file, String name) {
-
-    }
-    public static final int n=6;
-    public static int i,j,z,level;
-    public static int[] a = new int[n];
-    public static int[] d = new int[n];
-    public static void polyPhaseSort() throws IOException {
-//        final int n =6;
-        int mx,tn;
-        int k,dn;
-        int x,min;
+    public static void polyphaseSort(long eof) throws IOException {
+        int i, z, k, x, mx, min;
+        int tn,dn;
+        int[] a = new int[n];
+        int[] d = new int[n];
         int[] t = new int[n];
         int[] ta = new int[n];
-        File f0;
         File[] f = new File[n];
-        String[] fn = new String[n];
-        int eof = 5;
-        int c=0;
+        File f0 = new File("name.txt");
 
         FileWriter[] in = new FileWriter[n];
 
-//        openRandomSeq("gb",3);
-        f0 = new File("name.txt");
-        //listSeq();
-//            openSeq(f[i],fn[i]);
-        for (i=0;i<n;i++) {
-            fn[i]="T"+(i+1)+".txt";
-            f[i] = new File(fn[i]);
+        for(i=0;i<n-1;i++) {
+            f[i] = new File("T"+(i+1)+".txt");
             f[i].createNewFile();
-        }
-        for (i=0;i<n-1;i++) {
-            a[i]=1;d[i]=1;
-            //startWrite(f[i]);//!?
             in[i]= new FileWriter(f[i]);
         }
-        level=1;j=0;//!!!
-        a[n-1]=0;d[n-1]=0;//!!!
-//        startRead(f0);
+        f[n-1] = new File("T"+n+".txt");
+        f[n-1].createNewFile();
+        for(i=0;i<n-1;i++) {
+            a[i] = 1; d[i] = 1;
+        }
+        l = 1; j = 0; a[n-1] = 0; d[n-1] = 0;
+
         BufferedReader reader = Files.newBufferedReader(f0.toPath(), StandardCharsets.UTF_8);
         boolean eor;
+        int c=0;
         String firstX, firstZ;
         String[] firstY=new String[n];
         firstX = reader.readLine();
         do {
-            select();
+            select(a,d);
+            eor=false;
 //            copyrun(f0,f[j]);
             do {
 //                copy();
                 firstY[j]=firstX;
+//                System.out.println(j+" "+firstX);
                 in[j].write(firstX+'\n');
                 firstX = reader.readLine();
-                eor=false;
-                if(Integer.parseInt(firstX)<Integer.parseInt(firstY[j])) eor=true;
+                if(firstX!=null) {
+                    if (Integer.parseInt(firstX) < Integer.parseInt(firstY[j])) eor = true;
+                }
                 c++;
-            } while (!eor);//!?
+//                System.out.println("c = "+c+" eof = "+eof);
+            } while (!eor && c!=eof);//!?
         } while (c!=eof && (j!=n-2));//!?
         j=0;
-        while (c!=eof) {//!?
-            select();
+//        System.out.println(c);
+        while (c!=eof) {//!? //c<eof
+            select(a,d);
             if(Integer.parseInt(firstY[j]) <= Integer.parseInt(firstX)) {
 //            copyrun(f0,f[j]);
+                eor=false;
                 do {
 //                copy();
                     firstY[j]=firstX;
                     in[j].write(firstX+'\n');
                     firstX = reader.readLine();
-                    eor=false;
-                    if(Integer.parseInt(firstX)<Integer.parseInt(firstY[j])) eor=true;
+//                System.out.println("firstX = "+firstX);
+                    if(firstX!=null) {
+                        if (Integer.parseInt(firstX) < Integer.parseInt(firstY[j])) eor = true;
+                    }
                     c++;
-                } while (!eor);//!?
-                if(c!=eof) {//!?
+//                System.out.println("c = "+c+" eof = "+eof);
+                } while (!eor && c!=eof);//!?
+                if(c==eof) {//!?
                     d[j]=d[j]+1;
                 }
+                else {
 //            copyrun(f0,f[j]);
+                    eor = false;
+                    do {
+//                copy();
+                        firstY[j]=firstX;
+                        in[j].write(firstX+'\n');
+                        firstX = reader.readLine();
+//                System.out.println("firstX = "+firstX);
+                        if(firstX!=null) {
+                            if (Integer.parseInt(firstX) < Integer.parseInt(firstY[j])) eor = true;
+                        }
+                        c++;
+//                System.out.println("c = "+c+" eof = "+eof);
+                    } while (!eor && c!=eof);//!?
+                }
+            }
+            else {
+//            copyrun(f0,f[j]);
+                eor = false;
                 do {
 //                copy();
                     firstY[j]=firstX;
                     in[j].write(firstX+'\n');
                     firstX = reader.readLine();
-                    eor=false;
-                    if(Integer.parseInt(firstX)<Integer.parseInt(firstY[j])) eor=true;
-                } while (!eor);//!?
+//                System.out.println("firstX = "+firstX);
+                    if(firstX!=null) {
+                        if (Integer.parseInt(firstX) < Integer.parseInt(firstY[j])) eor = true;
+                    }
+                    c++;
+//                System.out.println("c = "+c+" eof = "+eof);
+                } while (!eor && c!=eof);//!?
             }
-//            copyrun(f0,f[j]);
-            do {
-//                copy();
-                firstY[j]=firstX;
-                in[j].write(firstX+'\n');
-                firstX = reader.readLine();
-                eor=false;
-                if(Integer.parseInt(firstX)<Integer.parseInt(firstY[j])) eor=true;
-            } while (!eor);//!?
         }
+reader.close();
         BufferedReader[] readers = new BufferedReader[n];
         for(i=0;i<n-1;i++) {//!!!
+            in[i].close();
             t[i] = i;
-//            startRead();
+////            startRead();
             readers[i] = Files.newBufferedReader(f[i].toPath(), StandardCharsets.UTF_8);
         }
         t[n-1] = n-1;
+
+//        for(i=0;i<n;i++) {
+//            in[i]= new FileWriter(f[i]);
+//        }
+int minTmp, close;
+        close=t[n-1];
+        for(i=0;i<n-1;i++) {
+            firstY[i]=readers[i].readLine();
+        }
         do {
+            in[t[n-1]]= new FileWriter(f[t[n-1]]);
             z=a[n-2]; d[n-1]=0;
-//            startWrite(f[t[n-1]],t[n-1]);!!!!!!!!!!!!
+//            for(i=0;i<n;i++) {
+//                System.out.println(t[i]);
+//            }
+//            for(i=0;i<n-1;i++) {
+//                firstY[i]=readers[i].readLine();
+//            }
             do {
                 k = 0;
                 for (i = 0; i < n - 1; i++) {//!!!
@@ -195,112 +184,107 @@ public class Main {
                 }
                 else {
                     do {
-                        i = 0; mx = 0; min = Integer.parseInt(readers[ta[0]].readLine()); //f[ta[0]].first;
+//                        if(l==1){
+//                            readers[2] = Files.newBufferedReader(f[t[close]].toPath(), StandardCharsets.UTF_8);
+//                            in[t[n-1]].write(readers[2].readLine()+"");
+//                            in[t[n-1]].close();
+//                        }
+                        for(i=0;i<n;i++) {
+                            System.out.println("ta["+i+"] = "+ta[i]);
+                        }
+                        System.out.println("k = "+k);
+                        eor=false;
+                        i = 0; mx = 0; min = Integer.parseInt(firstY[ta[0]]); //f[ta[0]].first;
+                        System.out.println("in file T"+(i+1)+" min = "+min);
                         while (i < k) {
-                            i = i + 1; x = Integer.parseInt(readers[ta[i]].readLine());//????
+                            i = i + 1; x = Integer.parseInt(firstY[ta[i]]);//????
+                            System.out.println("in file T"+(i+1)+" x = "+x);
                             if (x < min) {
                                 min = x; mx = i;
+                                System.out.println("New min = "+min);
                             }
                         }
+                        System.out.println("Final min is "+min);
 //                        copy(f[ta[mx]],f[t[n-1]]);
-                        firstX=readers[ta[mx]].readLine();
-                        firstZ=firstX;
-                        in[t[n-1]].write(firstX);
-                        eor=false;
-                        if(Integer.parseInt(firstX)<Integer.parseInt(firstZ)) eor=true;
-
-                        if (eor) {
-                            for (int tx = mx; tx < k; tx++) {
-                                ta[tx] = ta[tx + 1];
+                        System.out.println("mx = "+mx+"; file with min value T"+(ta[mx]+1));
+//                        firstX=readers[ta[mx]].readLine();
+//                        firstZ=firstX;
+                        in[t[n-1]].write(min+"\n");
+                        System.out.println(min+" wrote to T4");
+//                        firstY[mx]= String.valueOf(min);
+                        firstY[ta[mx]]= readers[ta[mx]].readLine();
+                        System.out.println("New arr of firstY:");
+                        for(i=0;i<n;i++) {
+                            System.out.println("firstY["+i+"] = "+firstY[i]);
+                        }
+//                        eor=false;
+                        if(firstY[ta[mx]]!=null) {
+                            System.out.println("mx = "+mx);
+                            if (min > Integer.parseInt(firstY[ta[mx]])) {
+                                eor = true;
+//                                firstY[ta[mx]]= String.valueOf(MAX_VALUE);
                             }
+                        }
+                        else {
+                            firstY[ta[mx]]= String.valueOf(MAX_VALUE);
+                            eor=true;
+                        }
+                        System.out.println(eor);
+//нужно как-то  сохранять значения, а когда нужно менять
+                        if (eor) {
+                            System.out.println("Seria stopped!");
+                            ta[mx]=ta[k];
+//                            for (int tx = mx; tx < k; tx++) {
+//                                ta[tx] = ta[tx + 1];
+//                            }
                             k = k - 1;
                         }
+                        System.out.println();
                     } while (k != 0);
                 }
+                System.out.println("k != "+k);
                 z = z - 1;
+                System.out.println("z = "+z);
+                System.out.println();
+                System.out.println();
             }while (z!=0);
-//            startRead(f[t[n-1]],t[n-1]);
             tn=t[n-1];dn=d[n-1];z=a[n-2];
             for (i=n-1;i>0;i--) {
                 t[i]=t[i-1];d[i]=d[i-1];a[i]=a[i-1]-z;
             }
             t[0]=tn;d[0]=dn;a[0]=z;
-//            startWrite(f[t[1]], t[n]);
-            in[t[1]].write(t[n-1]);
-            level=level-1;
-        } while (level!=0);
-        for (i=0;i<n;i++) {
-//            closeSeq(f[i]);
-            in[i].close();
-            readers[i].close();
-        }
-//        closeSeq(f0);
-        reader.close();
-        }
-
-
-
-    public static void straightMerge(int n, int[] a) {//как в учебнике!
-        int i,j, k, l,t;
-        int h,m, p ,q,r;
-        boolean up;
-
-        up = true;
-        p=1;
-        do{
-            h=1; m=n;
-            if(up) {i=0; j=n-1; k=n; l=2*n-1;}
-            else   {k=0; l=n-1; i=n; j=2*n-1;}
-            do{
-                if(m>=p) {q=p;}
-                else {q=m;}
-                m=m-q;
-                if(m>=p) {r=p;}
-                else {r=m;}
-                m=m-r;
-                while ((q!=0) && (r!=0)) {
-                    if(a[i]<a[j]){a[k]=a[i];k=k+h;i=i+1;q=q-1;}
-                    else         {a[k]=a[j];k=k+h;j=j-1;r=r-1;}
-                }
-                while (r>0)       {a[k]=a[j];k=k+h;j=j-1;r=r-1;}
-                while (q>0)       {a[k]=a[i];k=k+h;i=i+1;q=q-1;}
-                h=-h; t=k; k=l; l=t;
-            } while (m!=0);
-            up=!up;p=2*p;
-        } while (p<=n);
-        if(!up) {
+//            in[t[0]].write(t[n-1]);
+            l=l-1;
+            in[close].close();
+//            Scanner sc = new Scanner(System.in);
+//            sc.nextInt();
+//            for(i=0;i<n;i++) {
+//                System.out.println("t["+i+"] = "+t[i]);
+//            }
+            readers[t[n-1]].close();
+            readers[close] = Files.newBufferedReader(f[close].toPath(), StandardCharsets.UTF_8);
+            firstY[close] = readers[close].readLine();
+            close=t[n-1];
+            System.out.println("New arr of firstY:");
             for(i=0;i<n;i++) {
-                a[i]=a[i+n];
+                System.out.println("firstY["+i+"] = "+firstY[i]);
             }
-        }
+
+//            readers[close].close();
+//            readers[close] = Files.newBufferedReader(f[close].toPath(), StandardCharsets.UTF_8);
+//            for(i=0;i<=n-1;i++) {//!!!
+////            readers[i].close();
+//                in[i].close();
+//            }
+        } while (l!=0);
+        System.out.println("I`m here!");
+
+
+//        for(i=0;i<=n-1;i++) {//!!!
+////            readers[i].close();
+//            in[i].close();
+//        }
+        in[n-1].close();
+        System.out.println("Sorted in file T"+(t[0]+1));
     }
 }
-
-
-
-
-
-
-
-
-
-    /*public static void select() {
-
-    }
-    public static void copyrun() {
-
-    }
-    public static void polyphaseSort() {
-        final int n = 6;
-        int i,j, mx, tn;
-        int k, dn, z, level;
-        int x, min;
-        int[] a = new int[n];
-        int[] d = new int[n];
-        int[] t = new int[n];
-        int[] ta = new int[n];
-        Sequence f0;
-        Sequence[] f = new Sequence[n];
-        String[] fn = new String[n];
-
-    }*/
